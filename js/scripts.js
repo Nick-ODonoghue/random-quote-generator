4e1035b472b7ae0f7700b11be0ee34558982251c
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-12 19:34:23 +0100

    added updated screenshot of app. Finished scripts.js making sure app fully works and added comments on my work for furture reference

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Asynchronous function to get a new quote
     async function getQuote() {
 
-        // Using try catch
+        // Using try catch for API request
         try {
             const response = await fetch(url);
             const data = await response.json();
@@ -34,13 +34,14 @@ document.addEventListener("DOMContentLoaded", () => {
     // Event listener for button click
     btn.addEventListener("click", getQuote);
 
-    // Once page is loaded the getQuote function is called, so there will always be a quote on page
+    // Once page is loaded the getQuote function is called, so there will always be a quote on page. As well as the other function to make sure the page is working as intended
     getQuote();
     starHighlight();
     updateUI();
+    deleteFavourite();
 });
 
-// Funtion for adding favourite to localStorage
+// Funtion for adding favourite to local storage
 function addToFavourite() {
 
     // Grab DOM elements
@@ -53,39 +54,68 @@ function addToFavourite() {
     const authorText = author.textContent;
     const quoteText = quote.textContent;
 
-    // Create and update hash
+    // Create and update hash with favourite quote Author and Quote
     const favouriteHash = {};
     favouriteHash.author = authorText;
     favouriteHash.quote = quoteText;
 
-    // Create and update array
+    // Create and update array inserting hash
     const favouriteArray = [favouriteHash];
 
-    // Add to localStorage on click
+    // Add to local storage on click
     favourite.addEventListener("click", () => {
+
+        // Local storage only excepts string data, so using JSON.stringify method to add favouriteArray
         localStorage.setItem("favourite", JSON.stringify(favouriteArray));
-        star.classList.add("favourite");
+
+        // Call starHighlight() in oder for favourite css to take affect
         starHighlight();
     })
+}
+
+// Function to remove favourite from local storage
+function deleteFavourite() {
+
+    // Grab DOM element
+    const trashcan = document.querySelector(".fa-trash-can");
+
+    // on click event to delete local storage using key "favourite"
+    trashcan.addEventListener("click", () => {
+        localStorage.removeItem("favourite");
+
+        // Call starHighlight to remove favourite class from star icon
+        starHighlight()
+    });
 }
 
 // Function for highlighting star
 function starHighlight() {
 
+    // Grab DOM element
     const star = document.querySelector(".fa-star");
+
+    // Conditional logic to add or remove favourite class from star icon
     if (localStorage.length > 0) {
         star.classList.add("favourite");
+    } else {
+        star.classList.remove("favourite");
     }
-
 }
 
-// function to update UI with favourite quote if one is stored
+// Function to update UI with favourite quote if one is stored
 function updateUI() {
+
+    // If statment used with setTimeout to add users favourited quote on page load up, if one is saved in their local storage
     if (localStorage.length > 0) {
         setTimeout(() => {
+
+            // Grabbing & then parsing local storage string to JSON so the data can be used to add back to the UI
             const storedFavourite = JSON.parse(localStorage.favourite);
+
+            // Using .notation to insert the data
             quote.textContent = storedFavourite[0].quote;
             author.textContent = storedFavourite[0].author;
-        }, 500);
+
+        }, 1000);
     }
 }
